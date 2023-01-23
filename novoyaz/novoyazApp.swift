3bdadbfb1a5f7aa3d422c64c0664a37539d781d9
MihailKisluhin <89089003390@mail.ru>
2023-01-23 23:59:24 +0500

    Initial Commit

---
+++ novoyaz/novoyazApp.swift
@@ -0,0 +1,17 @@
+//
+//  novoyazApp.swift
+//  novoyaz
+//
+//  Created by Kisluhin Mihail on 23.01.23.
+//
+
+import SwiftUI
+
+@main
+struct novoyazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
