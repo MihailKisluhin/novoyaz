40abd09bb2548c82fe0a7fcffd2ccc1949d535fa
MihailKisluhin <89089003390@mail.ru>
2023-01-28 02:44:42 +0500

    27.01

--- novoyaz/DataModel/DataController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DataController.swift
-//  novoyaz
-//
-//  Created by Kisluhin Mihail on 27.01.23.
-//
-
-import Foundation
--- novoyaz/novoyazApp.swift
+++ novoyaz/novoyazApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct novoyazApp: App {
@@ -15,3 +16,36 @@ struct novoyazApp: App {
         }
     }
 }
+
+
+//
+//import SwiftUI
+//import FirebaseCore
+//content_copy
+//
+//
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//content_copy
+//
+//    return true
+//  }
+//}
+//
+//@main
+//struct YourApp: App {
+//  // register app delegate for Firebase setup
+//  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//content_copy
+//
+//
+//  var body: some Scene {
+//    WindowGroup {
+//      NavigationView {
+//        ContentView()
+//      }
+//    }
+//  }
+//}
